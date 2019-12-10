57d6efb5db1f96c5ac02ff9eef528795726d6117
shawn <shawn@little.novalocal>
2019-12-10 05:04:05 -0500

    works!

--- cipher/cipher-gcm-ppc.c
+++ cipher/cipher-gcm-ppc.c
@@ -73,8 +73,63 @@ vec_aligned_ld(unsigned long offset, const unsigned char *ptr)
 #endif
 }
 
-#define STORE_TABLE(slot, vec) \
-  (vec_aligned_st (((block)vec), slot * 16, (unsigned char *)(gcm_table)))
+#include <stdio.h>
+
+void hexDump (const char *desc, const void *addr, const int len) {
+    int i;
+    unsigned char buff[17];
+    const unsigned char *pc = (const unsigned char*)addr;
+
+    // Output description if given.
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+
+    if (len == 0) {
+        printf("  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0) {
+        printf("  NEGATIVE LENGTH: %i\n",len);
+        return;
+    }
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    // And print the final ASCII bit.
+    printf ("  %s\n", buff);
+}
+
+#define STORE_TABLE(gcm_table, slot, vec) \
+  vec_aligned_st (((block)vec), slot * 16, (unsigned char *)(gcm_table)); \
+  hexDump("", &vec, 16);
 
 
 static ASM_FUNC_ATTR_INLINE void
@@ -124,17 +179,22 @@ vec_load_be(unsigned long offset, const unsigned char *ptr,
  */
 void ASM_FUNC_ATTR 
 __attribute__((optimize(0)))
-_gcry_ghash_setup_ppc_vpmsum (uint64_t *gcm_table, void *gcm_key)
+_gcry_ghash_setup_ppc_vpmsum (volatile uint64_t *gcm_table, void *gcm_key)
 {
+  printf("%lx\n", gcm_table);
   volatile vector2x_u64 zero8 = {0, 0};
-  volatile vector16x_u8 bswap_const = { 12, 13, 14, 15, 8, 9, 10, 11, 3, 4, 5, 6, 0, 1, 2, 3 };
+  volatile vector16x_u8 bswap64_const = { 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7 };
+  volatile vector16x_u8 bswap_const = { 12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3 };
   volatile vector1x_u128 H = VEC_LOAD_BE(gcm_key, bswap_const);
 
   volatile vector16x_u8 c2, t0, t1, t2, most_sig_of_H, t4, t5, t6;
-  volatile vector2x_u64 in, in1;
+  volatile vector2x_u64 d0, d1, d2;
+  volatile vector2x_u64 in, in2;
   volatile vector2x_u64 H_lo = zero8, H_hi = zero8;
   volatile vector2x_u64 lo, mid, hi;
   volatile vector2x_u64 H2_lo = zero8, H2, H2_hi = zero8;
+  volatile vector2x_u64 X_lo, X2_lo, X_mid, X2_mid, X_hi, X2_hi,
+    reduce, reduce2;
 
   // This in a long sequence to create the following constant
   // { U64_C(0x0000000000000001), U64_C(0xc200000000000000) };
@@ -150,7 +210,8 @@ _gcry_ghash_setup_ppc_vpmsum (uint64_t *gcm_table, void *gcm_key)
   // rotate H
   t2 = vec_splat_u8(7);
   most_sig_of_H = vec_splat((vector16x_u8)H, 15);
-  H = H << (vector1x_u128)t1;
+  vector1x_u128 one = {1};
+  H = H << one; // vsl is a strange instruction, and I don't think it is modeled, but it can reuse t0 above
   most_sig_of_H = most_sig_of_H >> t2;
   most_sig_of_H = most_sig_of_H & c2;
   in = (vector2x_u64)(H ^ (vector1x_u128)most_sig_of_H);
@@ -159,42 +220,89 @@ _gcry_ghash_setup_ppc_vpmsum (uint64_t *gcm_table, void *gcm_key)
 
   H = (((vector1x_u128)in << 64) | ((vector1x_u128)in >> 64));
   H_lo[0] = ((vector2x_u64)H)[1];
-  H_hi[0] = ((vector2x_u64)H)[0];
+  H_hi[1] = ((vector2x_u64)H)[0];
 
-  STORE_TABLE(1, c2);
-  STORE_TABLE(2, H_lo);
-  STORE_TABLE(3, H);
-  STORE_TABLE(4, H_hi);
+  STORE_TABLE(gcm_table, 1, c2);
+  STORE_TABLE(gcm_table, 2, H_lo);
+  STORE_TABLE(gcm_table, 3, H);
+  STORE_TABLE(gcm_table, 4, H_hi);
 
   lo = asm_vpmsumd(H_lo, in); // do not need to mask in because 0 * anything -> 0
   mid = asm_vpmsumd((vector2x_u64)H, in);
   hi = asm_vpmsumd(H_hi, in);
 
   // reduce 1
-  t2 = asm_vpmsumd(lo, (vector2x_u64)c2);
+  d2 = asm_vpmsumd(lo, (vector2x_u64)c2);
 
-  t0 = mid << 64;
-  t1 = mid >> 64;
-  lo ^= t0;
-  hi ^= t1;
-  lo = (vector1x_u128)lo << 64 | (vector1x_u128)lo >> 64;
-  lo ^= t2;
+  d0 = (vector2x_u64)((vector1x_u128)mid << 64);
+  d1 = (vector2x_u64)((vector1x_u128)mid >> 64);
+  lo ^= d0;
+  hi ^= d1;
+  lo = (vector2x_u64)((vector1x_u128)lo << 64 | (vector1x_u128)lo >> 64);
+  lo ^= d2;
 
   // reduce 2
-  t1 = (vector1x_u128)lo << 64 | (vector1x_u128)lo >> 64;
-  low = asm_vpmsumd(lo, t2);
-  t1 ^= hi;
-  in1 = lo ^ t1;
+  d1 = (vector2x_u64)((vector1x_u128)lo << 64 | (vector1x_u128)lo >> 64);
+  lo = asm_vpmsumd(lo, (vector2x_u64)c2);
+  d1 ^= hi;
+  in2 = lo ^ d1;
 
-  H2 = ((vector1x_u128)in1 << 64) | ((vector1x_u128)in1 >> 64);
+  H2 = (vector2x_u64)(((vector1x_u128)in2 << 64) | ((vector1x_u128)in2 >> 64));
   H2_lo[0] = ((vector2x_u64)H2)[1];
-  H2_hi[0] = ((vector2x_u64)H2)[0];
+  H2_hi[1] = ((vector2x_u64)H2)[0];
 
-  STORE_TABLE(5, H2_lo);
-  STORE_TABLE(6, H2);
-  STORE_TABLE(7, H2_hi);
+  STORE_TABLE(gcm_table, 5, H2_lo);
+  STORE_TABLE(gcm_table, 6, H2);
+  STORE_TABLE(gcm_table, 7, H2_hi);
 
-  abort();
+  X_lo = asm_vpmsumd(H2_lo, in);
+  X2_lo = asm_vpmsumd(H2_lo, in2);
+  X_mid = asm_vpmsumd(H2, in);
+  X2_mid = asm_vpmsumd(H2, in2);
+  X_hi = asm_vpmsumd(H2_hi, in);
+  X2_hi = asm_vpmsumd(H2_hi, in2);
+
+  reduce = asm_vpmsumd(X_lo, (vector2x_u64)c2);
+  reduce2 = asm_vpmsumd(X2_lo, (vector2x_u64)c2);
+
+  X_lo ^= (vector2x_u64)((vector1x_u128)X_mid << 64);
+  X_hi ^= (vector2x_u64)((vector1x_u128)X_mid >> 64);
+  X2_lo ^= (vector2x_u64)((vector1x_u128)X2_mid << 64);
+  X2_hi ^= (vector2x_u64)((vector1x_u128)X2_mid >> 64);
+
+  H = ((vector1x_u128)X_lo << 64 | (vector1x_u128)X_lo >> 64);
+  H2[0] = ((vector2x_u64)X2_lo)[1];
+  H2[1] = ((vector2x_u64)X2_lo)[0];
+
+  H ^=  (vector1x_u128)reduce;
+  H2 ^= reduce2;
+
+  // We could have also b64 switched reduce and reduce2, however as we are
+  // using the unrotated H and H2 above to vpmsum, this is marginally better.
+  reduce[0] = ((vector2x_u64)H)[1];
+  reduce[1] = ((vector2x_u64)H)[0];
+  reduce2[0] = ((vector2x_u64)H2)[1];
+  reduce2[1] = ((vector2x_u64)H2)[0];
+
+  H =  (vector1x_u128)asm_vpmsumd((vector2x_u64)H, (vector2x_u64)c2);
+  H2 = asm_vpmsumd(H2, (vector2x_u64)c2);
+
+  reduce ^= X_hi;
+  reduce2 ^= X2_hi;
+  H ^= (vector1x_u128)reduce;
+  H2 ^= reduce2;
+
+  H_lo = (vector2x_u64)((vector1x_u128)H << 64);
+  H_hi = (vector2x_u64)((vector1x_u128)H >> 64);
+  H2_lo = (vector2x_u64)((vector1x_u128)H2 << 64);
+  H2_hi = (vector2x_u64)((vector1x_u128)H2 >> 64);
+
+  STORE_TABLE(gcm_table, 8, H_lo);
+  STORE_TABLE(gcm_table, 9, H);
+  STORE_TABLE(gcm_table, 10, H_hi);
+  STORE_TABLE(gcm_table, 11, H2_lo);
+  STORE_TABLE(gcm_table, 12, H2);
+  STORE_TABLE(gcm_table, 13, H2_hi);
 }
 
 unsigned int ASM_FUNC_ATTR
